42d02d06a641ba1b6f7a4ec616b05d00ef858d80
reedbowman1 <reedbowman@icloud.com>
2026-03-31 13:56:53 -0700

    Enhanced admin site and added debugtoolbar

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 import datetime
+
+from django.contrib import admin
 from django.utils import timezone
 
 from django.db import models
@@ -12,6 +14,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published Recently?"
+    )
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
