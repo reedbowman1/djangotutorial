e594bfe4d4de029c242d0100b88b41d423f26997
reedbowman1 <reedbowman@icloud.com>
2026-03-31 11:38:53 -0700

    added tests and restricted Question viewing functionality

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Question, Choice
@@ -16,12 +17,14 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def detail(request, question_id):
 #     # try:
