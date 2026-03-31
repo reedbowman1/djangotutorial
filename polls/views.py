9642ed0aaac472dba2c14e5c6e1e03f1b93050f3
reedbowman1 <reedbowman@icloud.com>
2026-03-31 11:15:15 -0700

    added standardized views

--- polls/views.py
+++ polls/views.py
@@ -35,7 +35,9 @@ class DetailView(generic.DetailView):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, "polls/results.html", {"question": question})
 
-class ResultsView(generic.DetailView)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
